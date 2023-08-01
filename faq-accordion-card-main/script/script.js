6666e4f54ff8fedfd0d9096e38de03ea14b80693
Nickolas Lopes <nickolaslopes.developer@gmail.com>
2023-07-31 22:48:09 -0300

    new project

---
+++ faq-accordion-card-main/script/script.js
@@ -0,0 +1,13 @@
+const content = document.querySelectorAll('section');
+for(let i = 0; i < content.length; i++){
+    content[i].addEventListener('click', function(){
+        console.log(content[i].children)
+        if(content[i].children[1].classList.contains('content')){
+            this.classList.remove('content')
+            this.classList.add('active')
+        }else{
+            this.classList.add('content')
+            this.classList.remove('active')
+        }
+    })
+}
