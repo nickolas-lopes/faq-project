0345e5fe6cbbfd26d2351b1c8c041c1d265145a5
Nickolas Lopes <nickolaslopes.developer@gmail.com>
2023-08-02 20:01:49 -0300

    changes

--- faq-accordion-card-main/script/script.js
+++ faq-accordion-card-main/script/script.js
@@ -1,14 +1,3 @@
-const content = document.querySelectorAll('section');
-const text = document.querySelectorAll('.content')
-for(let i = 0; i < content.length; i++){
-    content[i].addEventListener('click', function(){
-        console.log(content[i].children)
-        if(content[i].children[1].classList.contains('content')){
-            this.classList.remove('content')
-            this.classList.add('active')
-        }else{
-            this.classList.add('content')
-            this.classList.remove('active')
-        }
-    })
-}
+const questions = document.querySelectorAll('h5');
+function
+
