a85ade1c0a698188c03e0d43326497a836394f2f
Nickolas Lopes <140769499+nickolas-lopes@users.noreply.github.com>
2023-08-02 22:47:08 -0300

    Atualizar o script.js

--- faq-accordion-card-main/script/script.js
+++ faq-accordion-card-main/script/script.js
@@ -3,7 +3,7 @@ const texts = document.querySelectorAll('p')
 for(let i = 0; i < questions.length; i++){
     questions[i].addEventListener('click', function (){
         texts.forEach(text => {
-           
+           text.classList.toggle('content')
         }
     })
 }
